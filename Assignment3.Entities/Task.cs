a49a433f2986e86634626dddb33d927d982e926a
Laurits Brok <lauritsbrok@gmail.com>
2022-09-23 14:08:32 +0200

    "fixed errors with bang"

--- Assignment3.Entities/Task.cs
+++ Assignment3.Entities/Task.cs
@@ -6,19 +6,19 @@ public class Task
 {
     public int Id { get; init; }
     [Required, MaxLength(100)]
-    public string Title { get; set; }
-    public User User {get; set;}
+    public string Title { get; set; } = default!;
+    public User User {get; set;} = default!;
 
-    public string Description { get; set; }
+    public string Description { get; set; } = default!;
     [Required]
 
-    private String _State;
+    private String _State = default!; 
 
     [Column(TypeName = "nvarchar(24)")]
     public State State {get; set;}
-    public Tag Tag {get; set;}
+    public Tag Tag {get; set;} = default!;
 
-    public virtual ICollection<Tag> Tags { get; set; }
+    public virtual ICollection<Tag> Tags { get; set; } = default!;
 }
 
 public enum State{
